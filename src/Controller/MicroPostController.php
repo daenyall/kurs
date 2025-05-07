ee3f3e94a6a150f7a3adba58d6f3463e97ccbf91
daniel <dankad767@gmail.com>
2025-05-07 13:43:28 +0200

    added basic form

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -6,7 +6,11 @@ use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 final class MicroPostController extends AbstractController
 {
@@ -25,5 +29,32 @@ final class MicroPostController extends AbstractController
         return $this->render('micro_post/show.html.twig', [
             'post' => $post,
         ]);
+    } 
+    #[Route('/micro-post/add', name:'app_micro_post_add', priority: 2 )]
+    public function add(Request $request, MicroPostRepository $posts): Response
+    {
+        $microPost = new MicroPost();
+        $form = $this->createFormBuilder($microPost)
+        ->add ('title', TextType::class)
+        ->add('text', TextType::class)
+        ->add('submit', SubmitType::class, ['label' => 'Save'])
+        ->getForm();
+
+        $form->handleRequest($request);
+        if( $form->isSubmitted() && $form->isValid() ) 
+        {
+            $post = $form->getData();
+            $post->setCreated(new \DateTime());
+            $posts->add($post, true);
+            dd($post);
+            // add a flash msg 
+            // redirect
+        }
+        return $this->render(
+            'micro_post/add.html.twig',
+            [
+                'form'=> $form,
+            ]
+        );
     }
 }
